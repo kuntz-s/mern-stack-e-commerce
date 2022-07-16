2829f5c702920d33d12457ac0b6e66f27a54411a
kuntz stephane nchouwet <kuntzstephanenchouwetmfouapon@gmail.com>
2022-07-16 21:18:45 +0100

    remove bootstrap library and installing tailwind

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+const logo = require("./assets/logo.png")
 
 const App = () => {
   return (
     <>
       <Header/>
       <main>
-        <h1>MERN STACK E-COMMERCE</h1>
+        <h1>MERN STACK E-COMMERCEs </h1>
+        <img src={logo}  alt="logo" style={{backgroundColor:"yellow",color:"red"}}/>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="bg-violet-500 hover:bg-violet-600 active:bg-green-700 focus:outline-none focus:ring focus:ring-violet-300">
+      Bouton
+    </button>
       </main>
       <Footer/>
     </>
