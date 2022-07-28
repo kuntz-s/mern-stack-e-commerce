37a9fc2ab7de825f8f502cf2c75c9b655599dae8
kuntz stephane nchouwet <kuntzstephanenchouwetmfouapon@gmail.com>
2022-07-28 22:45:40 +0100

    implementing the creation of a jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 
 /**
@@ -19,7 +20,7 @@ const authUser = (asyncHandler (async (req, res) => {
             surname: user.surname,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
    }else {
         res.status(401);
