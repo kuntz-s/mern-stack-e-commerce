fec37e787db51147f2e739e0c56ec7be9593c051
kuntz stephane nchouwet <kuntzstephanenchouwetmfouapon@gmail.com>
2022-08-08 21:41:22 +0100

    modifying category model and adding category data

---
+++ backend/data/categories.js
@@ -0,0 +1,42 @@
+//name, url
+
+const categoriesData = [
+  {
+    name: "Telephones",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-phone_1024x1024_crop_center.jpg?v=1616408588",
+  },
+  {
+    name: "Laptops et tablettes",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-lap_1024x1024_crop_center.jpg?v=1616408604",
+  },
+  {
+    name: "Televisions",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-tivi_1024x1024_crop_center.jpg?v=1616410673",
+  },
+  {
+    name: "Pc et accessoires",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-pc_1024x1024_crop_center.jpg?v=1616410730",
+  },
+  {
+    name: "Audio et vidéos",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-audio_1024x1024_crop_center.jpg?v=1616410745",
+  },
+  {
+    name: "Appareils electromenagers",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-fridge_1024x1024_crop_center.jpg?v=1616410888",
+  },
+  {
+    name: "Jeux",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-game_1024x1024_crop_center.jpg?v=1616410761",
+  },
+  {
+    name: "Caméras",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-camera_1024x1024_crop_center.jpg?v=1616410914",
+  },
+  {
+    name: "Accessoires",
+    url: "https://cdn.shopify.com/s/files/1/0064/4435/1539/files/icon-headphone_1024x1024_crop_center.jpg?v=1616410951",
+  }
+];
+
+export default categoriesData;
