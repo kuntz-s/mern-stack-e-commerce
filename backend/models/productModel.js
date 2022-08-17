3ecd29a94b27ee29906a57683ab67f4be3c44bae
kuntz stephane nchouwet <kuntzstephanenchouwetmfouapon@gmail.com>
2022-08-17 18:01:00 +0100

    add fetch products by popularity, recent, brand , category

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -58,6 +58,8 @@ const productSchema = new mongoose.Schema(
       type: Number,
       required: true,
       default: 0,
+      min:0,
+      max:5
     },
     reviews: [reviewSchema],
     numReviews: {
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,16 +2,25 @@ import express from "express";
 import {
   getProductById,
   getAllProducts,
-  addProduct
+  addProduct,
+  getPopularProducts,
+  getProductsByCategory,
+  getNewestProducts,
+  getProductsByBrand
 } from "../controllers/productController.js";
-import {protect, verifyAdmin} from "../middleware/authMiddleware.js";
+import { protect, verifyAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/")
-  .get(getAllProducts)
-  .post(protect, verifyAdmin, addProduct)
-;
+router.get("/popular", getPopularProducts);
+
+router.get("/new", getNewestProducts);
+
+router.get("/category/:id", getProductsByCategory);
+
+router.get("/brand/:id", getProductsByBrand);
+
+router.route("/").get(getAllProducts).post(protect, verifyAdmin, addProduct);
 
 router.route("/:id").get(getProductById);
 
